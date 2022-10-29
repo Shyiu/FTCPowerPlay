3d7ec8c097eac2db5913843834c11fb98c6cc63b
Datlightning <79178850+Datlightning@users.noreply.github.com>
2022-10-29 14:08:29 -0400

    Added Set Power For Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMecanumDrive.java
@@ -45,10 +45,17 @@ public class ManualMecanumDrive extends LinearOpMode {
         Servo claw = hardwareMap.get(Servo.class, "claw_servo");
 
         armJoint1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        double clawOpen = .16;
-        double clawClose = -.5;
+        double clawOpen = .78;
+        double clawClose = 1;
         double lockJoint2 = 0;
-
+        double armUp = 1;
+        double armDown = 0;
+        double armJoint2Max = 1;
+        double armJoint2Min = 0;
+        double y,x,rx,y2;
+        double denominator,frontLeftPower,backLeftPower,frontRightPower,backRightPower;
+        double incrementWait = .5;
+        double armJoint2CurrentPos = armJoint2.getPosition();
         claw.setPosition(clawClose);
 
         waitForStart();
@@ -58,30 +65,12 @@ public class ManualMecanumDrive extends LinearOpMode {
         while (opModeIsActive()) {
 
             //Function of Game Controller 1
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
-            double y2 = -gamepad2.left_stick_y;
+            y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            rx = gamepad1.right_stick_x;
+            y2 = -gamepad2.left_stick_y;
             Servo.Direction direction = Servo.Direction.FORWARD;
 
-            double positionSave = 0;
-            double joint2Position;
-            if (lockJoint2 == 0) {
-                joint2Position = gamepad2.right_trigger;
-            }else{
-                joint2Position = positionSave;
-            }
-            if (gamepad2.a){
-                positionSave = gamepad2.right_trigger;
-                if (lockJoint2 == 0){
-                    lockJoint2 = 1;
-                    sleep(30);
-                }
-                else{
-                    lockJoint2 = 0;
-                    sleep(30);
-                }
-            }
 
 
 
@@ -89,11 +78,11 @@ public class ManualMecanumDrive extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            frontLeftPower = (y + x + rx) / denominator;
+            backLeftPower = (y - x + rx) / denominator;
+            frontRightPower = (y - x - rx) / denominator;
+            backRightPower = (y + x - rx) / denominator;
 
 
 
@@ -107,12 +96,33 @@ public class ManualMecanumDrive extends LinearOpMode {
 
 
             armJoint1.setPower(Math.pow(y2,2));
-            if (Math.abs(y2) > 0.25){
-                armJoint2.setPosition(armJoint2.getPosition()-(y2/10.0));
+            if((gamepad2.left_trigger) > 0){
+                armJoint2CurrentPos = armJoint2.getPosition();
+                if (armJoint2CurrentPos + .1 <= armJoint2Max){
+                     armJoint2CurrentPos += .1;
+                }
+                else{
+                    armJoint2CurrentPos = armJoint2Max;
+                }
+
+                incrementWait = 1.001 - gamepad2.left_trigger;
+                armJoint2.setPosition(armJoint2CurrentPos);
+                sleep((long)(incrementWait*1000));
             }
-            else{
-                armJoint2.setPosition(joint2Position);
+            else if((gamepad2.right_trigger) > 0){
+                armJoint2CurrentPos = armJoint2.getPosition();
+                if (armJoint2CurrentPos - .1 >= armJoint2Min){
+                    armJoint2CurrentPos -= .1;
+                }
+                else{
+                    armJoint2CurrentPos = armJoint2Min;
+                }
+
+                incrementWait = 1.001 - gamepad2.right_trigger;
+                armJoint2.setPosition(armJoint2CurrentPos);
+                sleep((long)(incrementWait*1000));
             }
+
 
             claw.setDirection(direction);
             if (gamepad2.y) {
