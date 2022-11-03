a7cf1c030359882d0e58a85f664252813b2c6ad6
Datlightning <79178850+Datlightning@users.noreply.github.com>
2022-11-02 22:21:40 -0400

    Wrote untested multithreading TR code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TileRunnerMultiThreading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TileRunnerMultiThreading.java
@@ -1,15 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
-public class TileRunnerMultiThreading extends LinearOpMode {
+import org.firstinspires.ftc.teamcode.threadopmode.TaskThread;
+import org.firstinspires.ftc.teamcode.threadopmode.ThreadOpMode;
 
-    // Initializes all four motors used in the drive train
-    // This code will be using a Tank-drive system
+@TeleOp
+public class TileRunnerMultiThreading extends ThreadOpMode {
     protected DcMotor frontRight;
     protected DcMotor backRight;
     protected DcMotor frontLeft;
@@ -18,18 +17,34 @@ public class TileRunnerMultiThreading extends LinearOpMode {
     protected Servo armJoint2;
     protected Servo claw;
 
-    final int[] LEVELS = {0,(1120/36),1000,1500};
+    final int[] LEVELS = {86, 1008, 1540};
+    final double[] SERVO_POS = {.58,.22,0};
+    int servo_position = 0;
     int current_level = 0;
-    public void Init(){
-        double clawOpen = .78;
-        double clawClose = 1;
+    @Override
+    public void mainInit() {
         armJoint1 = hardwareMap.get(DcMotor.class, "joint_motor");
         armJoint2 = hardwareMap.get(Servo.class, "joint_servo");
         claw = hardwareMap.get(Servo.class, "claw_servo");
 
         armJoint1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        double clawOpen = .78;
+        double clawClose = 1;
+        double armJoint2Max = 1;
+        double armJoint2Min = 0;
+        double armJoint2Increment = .025;
+        double incrementWait = .5;
+        double armJoint2CurrentPos = armJoint2.getPosition();
+        double armJoint1CurrentPos = armJoint1.getCurrentPosition();
+
+        armJoint1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armJoint1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        double armJoint1Min = armJoint1CurrentPos;
         claw.setPosition(clawClose);
 
+
+        // Pulls the motors from the robot configuration so that they can be manipulated
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
@@ -42,161 +57,89 @@ public class TileRunnerMultiThreading extends LinearOpMode {
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
 
+
+
         // Makes the Driver Hub output the message "Status: Initialized"
-
-
-
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-    }
-    public void gamepad2Controls(){
 
-        sleep(500);
-        double clawOpen = .78;
-        double clawClose = 1;
-
-        double armJoint2Max = 1;
-        double armJoint2Min = 0;
-        double armJoint2Increment = .01;
-        double y2;
-        double incrementWait = .5;
-        double armJoint2CurrentPos = 0;
-        while (opModeIsActive()){
-            Servo.Direction direction = Servo.Direction.FORWARD;
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                //The loop method should contain loop code
+                double leftTgtPower = -gamepad1.left_stick_y;
+                double rightTgtPower = -gamepad1.right_stick_y;
+                frontLeft.setPower(leftTgtPower);
+                backLeft.setPower(leftTgtPower);
+                frontRight.setPower(rightTgtPower);
+                backRight.setPower(rightTgtPower);
 
 
-            y2 = -gamepad2.left_stick_y;
-            armJoint1.setPower(-negSqrt(y2));
-            if((gamepad2.left_trigger) > 0){
-                armJoint2CurrentPos = armJoint2.getPosition();
-                if (armJoint2CurrentPos + armJoint2Increment <= armJoint2Max){
-                    armJoint2CurrentPos += armJoint2Increment;
+                // Makes the Driver Hub output the message
+                // Left Target Power: A float from [-1,1]
+                // Right Target Power: A float from [-1,1]
+                // The current power of each motor
+                telemetry.addData("Left Target Power", leftTgtPower);
+                telemetry.addData("Right Target Power", rightTgtPower);
+                telemetry.addData("Front Right Motor Power", frontRight.getPower());
+                telemetry.addData("Front Left Motor Power", frontLeft.getPower());
+                telemetry.addData("Back Right Motor Power", backRight.getPower());
+                telemetry.addData("Back Left Motor Power", backLeft.getPower());
+
+
+
+
+                // Status: Running
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+
+            }
+        }));
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            @Override
+            public void loop() {
+                double y2 = gamepad2.left_stick_y;
+
+
+                armJoint1.setPower(negSqrt(y2));
+
+
+
+                if (gamepad2.dpad_up){
+                    if (servo_position != SERVO_POS.length - 1){
+                        servo_position++;
+                    }
+                    else{
+                        servo_position=0;
+                    }
+                    armJoint2.setPosition(SERVO_POS[servo_position]);
+                    while (gamepad2.dpad_up){
+                        ;
+                    }
                 }
-                else{
-                    armJoint2CurrentPos = armJoint2Max;
+                if (gamepad2.dpad_down){
+                    servo_position--;
+                    if (servo_position < 0){
+                        servo_position = 0;
+                    }
+                    armJoint2.setPosition(SERVO_POS[servo_position]);
+                    while (gamepad2.dpad_down){
+                        ;
+                    }
                 }
 
-                incrementWait = 1.001 - gamepad2.left_trigger;
-                armJoint2.setPosition(armJoint2CurrentPos);
-                sleep((long)(incrementWait*1000));
-            }
-            else if((gamepad2.right_trigger) > 0){
-                armJoint2CurrentPos = armJoint2.getPosition();
-                if (armJoint2CurrentPos - armJoint2Increment >= armJoint2Min){
-                    armJoint2CurrentPos -= armJoint2Increment;
+
+                if (gamepad2.y) {
+                    claw.setPosition(clawOpen);
+                    armJoint2.setPosition(1);
+                    servo_position = 0;
                 }
-                else{
-                    armJoint2CurrentPos = armJoint2Min;
+                else if(gamepad2.x){
+                    claw.setPosition(clawClose);
                 }
-
-                incrementWait = 1.001 - gamepad2.right_trigger;
-                armJoint2.setPosition(armJoint2CurrentPos);
-                sleep((long)(incrementWait*1500));
             }
-
-
-            claw.setDirection(direction);
-            if (gamepad2.y) {
-                claw.setPosition(clawOpen);
-            }
-            else if(gamepad2.x){
-                claw.setPosition(clawClose);
-            }
-        }
-    }
-    public void gamepad1Controls() {
-        sleep(250);
-        // Pulls the motors from the robot configuration so that they can be manipulated
-
-
-
-        // Wait for the game to start (driver presses PLAY)
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            // Takes the current y-position of both the left and right joy-sticks
-            // The highest position of a joystick is equal to -1, and the bottommost position of the
-            // joystick is equal to 1
-            double leftTgtPower = -this.gamepad1.left_stick_y;
-            double rightTgtPower = -this.gamepad1.right_stick_y;
-
-
-//            if (gamepad2.dpad_up || current_level == 10){
-//                if(current_level < LEVELS.length -1)  {
-//                    current_level++;
-//                }
-//                slides.setTargetPosition(LEVELS[current_level]);
-//                slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//                slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            }
-//            else if (gamepad2.dpad_down || current_level == 10){
-//                if(current_level > 0) {
-//                    current_level--;
-//                }
-//                slides.setTargetPosition(LEVELS[current_level]);
-//                slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            }
-//            else if (Math.abs(gamepad2.left_stick_y) > 0){
-//                slides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//                slides.setPower(-1*(negSqrt(gamepad2.left_stick_y)/2.0));
-//            }
-//            else{
-//                slides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//                slides.setPower(0);
-//            }
-
-
-
-
-            // Uses the current position of the joy-sticks and makes the motors move at the
-            // respective speed
-            frontLeft.setPower(leftTgtPower);
-            backLeft.setPower(leftTgtPower);
-            frontRight.setPower(rightTgtPower);
-            backRight.setPower(rightTgtPower);
-
-
-            // Makes the Driver Hub output the message
-            // Left Target Power: A float from [-1,1]
-            // Right Target Power: A float from [-1,1]
-            // The current power of each motor
-            telemetry.addData("Left Target Power", leftTgtPower);
-            telemetry.addData("Right Target Power", rightTgtPower);
-
-            telemetry.addData("Front Right Motor Power", frontRight.getPower());
-            telemetry.addData("Front Left Motor Power", frontLeft.getPower());
-            telemetry.addData("Back Right Motor Power", backRight.getPower());
-            telemetry.addData("Back Left Motor Power", backLeft.getPower());
-
-
-
-            // Status: Running
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-
-        }
-
-    }
-    @Override
-    public void runOpMode()
-    {
-        Thread gp1 = new Thread(){
-            public void run(){
-                gamepad1Controls();
-            }
-        };
-        Thread gp2 = new Thread(){
-            public void run(){
-                gamepad2Controls();
-            }
-        };
-        Init();
-        waitForStart();
-        gp1.start();
-        gp2.start();
+        }));
     }
     public static double negSqrt(double value){
         if(value >= 0){
@@ -206,4 +149,9 @@ public class TileRunnerMultiThreading extends LinearOpMode {
             return -1 * Math.sqrt((-1*value));
         }
     }
+    @Override
+    public void mainLoop() {
+
+    }
+
 }
