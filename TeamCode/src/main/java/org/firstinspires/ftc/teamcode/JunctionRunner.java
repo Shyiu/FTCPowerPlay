b39d534c2106eadf4b7acd80c10e0bd1d4520f40
Datlightning <79178850+Datlightning@users.noreply.github.com>
2023-03-07 14:57:58 -0500

    Added drive control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JunctionRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JunctionRunner.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -12,13 +14,23 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 
 @Autonomous(name = "Junction Detection")
-
+@Config
 public class JunctionRunner extends LinearOpMode {
 
     OpenCvCamera camera;
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+    public static double speed = .3;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Junction"), cameraMonitorViewId);
@@ -41,15 +53,27 @@ public class JunctionRunner extends LinearOpMode {
         telemetry.setMsTransmissionInterval(50);
 
         waitForStart();
-        switch (detector.getLocation()) {
-            case TARGET:
-                // ...
-                break;
-            case UP:
-                // ...
-                break;
-            case NOT_FOUND:
-                // ...
+        while (!isStopRequested() && opModeIsActive()) {
+            switch (detector.getLocation()) {
+                case TARGET:
+                    frontLeft.setPower(0);
+                    frontRight.setPower(0);
+                    backLeft.setPower(0);
+                    backRight.setPower(0);
+                    break;
+                case UP:
+                    frontRight.setPower(speed);
+                    backRight.setPower(speed);
+                    frontLeft.setPower(-speed);
+                    backLeft.setPower(-speed);
+                    break;
+                case BELOW:
+                    frontRight.setPower(-speed);
+                    backRight.setPower(-speed);
+                    frontLeft.setPower(speed);
+                    backLeft.setPower(speed);
+                    break;
+            }
         }
         camera.stopStreaming();
     }
