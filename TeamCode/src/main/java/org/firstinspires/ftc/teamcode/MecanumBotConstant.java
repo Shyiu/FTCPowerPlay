285bc6dac4ae3efe34bf85e01d907b3528176fce
Datlightning <79178850+Datlightning@users.noreply.github.com>
2023-03-07 14:57:41 -0500

    Added Config data in comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumBotConstant.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumBotConstant.java
@@ -4,23 +4,23 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class MecanumBotConstant {
-    public static String fr = "frontRight";
-    public static String fl = "frontLeft";
-    public static String bl = "backLeft";
-    public static String br = "backRight";
+    public static String br = "backRight"; //Control Hub 0 20
+    public static String fr = "frontRight";//Control Hub 1 20
+    public static String bl = "backLeft";  //Control Hub 2 20
+    public static String fl = "frontLeft"; //Control Hub 3 20
     public static String intake = "flapper";
-    public static String right_servo = "left";
-    public static String left_servo = "right";
-    public static double TOP_HARDSTOP = 7350;
-    public static double BOTTOM_HARDSTOP = -498;
-    public static double[] SLIDE_POSITIONS = {BOTTOM_HARDSTOP, 2761, 5020, TOP_HARDSTOP};
-    public static double[] CONE_STACK_LEVELS = {-498, -498, 350, 650, 854}; //Find these values
-    public static String slides = "slides";
+    public static String left_servo = "right";//Control Hub 4
+    public static String right_servo = "left";//Control Hub 5
+    public static double TOP_HARDSTOP = 4300;
+    public static double BOTTOM_HARDSTOP = -550;
+    public static double[] SLIDE_POSITIONS = {BOTTOM_HARDSTOP, 1615, 3023, TOP_HARDSTOP};
+    public static double[] CONE_STACK_LEVELS = {-498, -222, -65, 159, 310}; //Find these values
+    public static String slides = "slides";//Expansion Hub 0  40
     public static String cameraApril = "Webcam 1";
     public static String cameraJunction = "Junction";
     public static String imu = "imu";
     public static String distance = "sensor_distance";
-    public static String color = "sensor_color";
+    public static String color = "sensor_color";//I2C 1 Control Hub Color/Range Sensor
     public static double rightServoOpenPosition = -0.175, leftServoOpenPosition = -0.068;
     public static double rightServoClosedPosition = -.025, leftServoClosedPosition = -.23;
     public static double rightHardStopOut = -.575;
